b5d3f842b6bde63208edbc2932f96fd1d35a58df
Andrija <akinovak@gmail.com>
2025-03-03 10:29:17 +0100

    more efficient mtree

--- examples/tree.jl
+++ examples/tree.jl
@@ -3,33 +3,12 @@ using ..CryptoUtilities
 
 leaves = ["1", "2", "3", "4", "5", "6", "7", "8"]
 tree = build_merkle_tree(leaves)
-for (i, layer) in enumerate(tree)
-    println("Layer $i: ", layer)
-end
-# merkle_root = get_root(tree)
-# println("Merkle Root: ", merkle_root)
+depth = get_depth(tree)
+root = get_root(tree)
 
-path = get_path(3, 3)
-# for pp in path
-#     println(pp)
-# end
+queries = [0, 2, 5, 7]
+proof = create_proof(tree, queries)
 
-layers = batch_paths(3, [6, 7, 3])
-# Layer 1: Dict(5 => 1, 4 => 3, 8 => 2)
-# Layer 2: Dict(4 => 1, 3 => 2, 1 => 3)
-# Layer 3: Dict(2 => 2, 1 => 1)
-# for i in 1:3
-#     println("Layer $i: ", layers[i])
-# end
-
-(shifts, proof_size) = flatten_layers(layers)
-
-println(shifts)
-println(proof_size)
-
-batched_proof = batch_proofs(3, tree, [6, 7, 3])
-println(batched_proof)
-
-opened_leaves = [leaves[6], leaves[7], leaves[3]]
-res = verify_batched_proof(3, get_root(tree), opened_leaves, [6, 7, 3], batched_proof)
-println(res)
+queried_leaves = [leaves[q + 1] for q in queries]
+res = verify_proof(root, depth, queried_leaves, queries, proof)
+@assert res == true
--- src/merkletree.jl
+++ src/merkletree.jl
@@ -1,5 +1,5 @@
 using SHA
-function hash_children(left::String, right::String)
+function hash_pair(left::String, right::String)
     return bytes2hex(sha256(left * right))
 end
 
@@ -16,7 +16,7 @@ function build_merkle_tree(data::Vector{String})
         for i in 1:2:length(current_layer)
             left = current_layer[i]
             right = current_layer[i + 1]
-            push!(next_layer, hash_children(left, right))
+            push!(next_layer, hash_pair(left, right))
         end
         push!(tree, next_layer)
         current_layer = next_layer
@@ -29,107 +29,112 @@ function get_root(tree::Vector{Vector{String}})
     return isempty(tree) ? "" : tree[end][1]
 end
 
-function get_path(depth::Int, query::Int)
-    path = Vector{Int}(undef, depth)
-
-    for i in 1:depth
-        mod = query % 2
-        path[i] = mod == 1 ? query + 1 : query - 1
-        query = (query + mod) ÷ 2
-    end
-
-    return path
+function get_depth(tree)
+    return length(tree) - 1
 end
 
-function batch_paths(depth::Int, queries::Vector{Int})
-    layers = [Dict{Int, Int}() for _ in 1:depth]
+function ith_layer!(current_layer, queries_len, queries, proof)
+    next_queries_len = 0
+    i = 1
 
-    for q in queries
-        path = get_path(depth, q)
+    while i <= queries_len
+        query = queries[i]
+        sibling = query ⊻ 1
 
-        for i in 1:depth
-            if !haskey(layers[i], path[i])
-                layers[i][path[i]] = length(layers[i]) + 1
+        next_queries_len += 1
+        queries[next_queries_len] = query >> 1
+
+        if i == queries_len
+            push!(proof, current_layer[sibling + 1])
+            break
+        end
+
+        if query % 2 != 0
+            push!(proof, current_layer[sibling + 1])
+            i += 1
+        else
+            if queries[i + 1] != sibling
+                push!(proof, current_layer[sibling + 1])
+                i += 1
+            else
+                i += 2
             end
         end
     end
 
-    return layers
+    return next_queries_len
 end
 
-function flatten_layers(layers::Vector{Dict{Int, Int}})
-    shifts = Vector{Int}(undef, length(layers))
-    proof_size = 0
+function create_proof(tree_levels, queries)
+    proof = String[]
+    depth = length(tree_levels) - 1
 
-    for i in 1:length(layers)
-        shifts[i] = proof_size
-        proof_size += length(layers[i])
-    end
-
-    return (shifts, proof_size)
-end
-
-# PROVER
-function batch_proofs(depth::Int, tree_levels::Vector{Vector{String}}, queries::Vector{Int})
-    layers = batch_paths(depth, queries)
-    (shifts, proof_size) = flatten_layers(layers)
-
-    proof = Vector{String}(undef, proof_size)
+    queries_buff = copy(queries)
+    queries_cnt = length(queries)
     for i in 1:depth
-        for (k, v) in layers[i]
-            idx = v + shifts[i]
-            proof[idx] = tree_levels[i][k]
-        end
+        queries_cnt = ith_layer!(tree_levels[i], queries_cnt, queries_buff, proof)
     end
 
     return proof
 end
 
-# VERIFIER 
-function verify_merkle_proof(leaf::String, proof::Vector{String}, root::String, query::Int)
-    current_hash = bytes2hex(sha256(leaf))
-    for sibling_hash in proof
-        mod = query % 2
-        if mod == 1
-            current_hash = hash_children(current_hash, sibling_hash)
-        else
-            current_hash = hash_children(sibling_hash, current_hash)
+function verify_ith_layer!(layer, queries, curr_cnt, proof, proof_cnt)
+    next_cnt = 0
+    i = 1
+
+    while i <= curr_cnt
+        query = queries[i]
+        sibling = query ⊻ 1
+
+        next_cnt += 1
+        queries[next_cnt] = query >> 1
+
+        if i == curr_cnt
+            proof_cnt += 1
+            pp = proof[proof_cnt]
+            if query % 2 != 0
+                layer[next_cnt] = hash_pair(pp, layer[i])
+            else
+                layer[next_cnt] = hash_pair(layer[i], pp)
+            end
+            break
         end
-        query = (mod + query) ÷ 2
-    end
-    return current_hash == root
-end
 
-function index_batched_proof(depth::Int, layers::Vector{Dict{Int, Int}}, shifts::Vector{Int}, path::Vector{Int}, batched_proof::Vector{String})
-    reconstructed_proof = Vector{String}(undef, depth)
-
-    for i in 1:depth
-        layer_idx = layers[i][path[i]]
-        abs_idx = layer_idx + shifts[i]
-        reconstructed_proof[i] = batched_proof[abs_idx]
-    end
-
-    return reconstructed_proof
-end
-
-function verify_batched_proof(depth::Int, root::String, leaves::Vector{String}, queries::Vector{Int}, batched_proof::Vector{String})
-    layers = batch_paths(depth, queries)
-    (shifts, proof_size) = flatten_layers(layers)
-
-    @assert length(batched_proof) == proof_size
-    @assert length(leaves) == length(queries)
-
-    # For each leaf
-    for i in 1:length(leaves)
-        # Get path for leaf
-        path = get_path(depth, queries[i])
-        # Verify opening proof from reconstructed proof
-        reconstructed_proof = index_batched_proof(depth, layers, shifts, path, batched_proof)
-        @assert verify_merkle_proof(leaves[i], reconstructed_proof, root, queries[i])
+        if query % 2 != 0
+            proof_cnt += 1
+            pp = proof[proof_cnt]
+            layer[next_cnt] = hash_pair(pp, layer[i])
+            i += 1
+        else
+            if queries[i + 1] != sibling
+                proof_cnt += 1
+                pp = proof[proof_cnt]
+                layer[next_cnt] = hash_pair(layer[i], pp)
+                i += 1
+            else
+                layer[next_cnt] = hash_pair(layer[i], layer[i + 1])
+                i += 2
+            end
+        end
     end
 
-    return true
+    return (next_cnt, proof_cnt)
 end
 
-export hash_children, build_merkle_tree, get_root, get_path, batch_paths, flatten_layers
-export batch_proofs, verify_merkle_proof, index_batched_proof, verify_batched_proof
+function verify_proof(root, depth, leaves, leaf_queries, batched_proof)
+    proof = copy(batched_proof)
+    layer = [bytes2hex(sha256(l)) for l in leaves]
+    queries = copy(leaf_queries)
+
+    curr_cnt = length(queries)
+    proof_cnt = 0
+
+    for _ in 1:depth
+        (curr_cnt, proof_cnt) = verify_ith_layer!(layer, queries, curr_cnt, proof, proof_cnt)
+    end
+
+    return layer[1] == root
+end
+
+export build_merkle_tree, get_root, get_depth
+export create_proof, verify_proof
