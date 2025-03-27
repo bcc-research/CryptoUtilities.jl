755a4bf90bad6c383a5e79c5a8bdf8a7b751566d
Andrija <akinovak@gmail.com>
2025-03-27 22:16:13 +0100

    utils

--- BinaryReedSolomon/src/BinaryReedSolomon.jl
+++ BinaryReedSolomon/src/BinaryReedSolomon.jl
@@ -2,7 +2,9 @@ module BinaryReedSolomon
 
 using BinaryFields
 
+# Utility functions
 is_pow_2(n) = 2^(round(Int, log2(n))) == n
+
 
 export fft!, ifft!, compute_twiddles, compute_twiddles!
 include("./binaryfft.jl")
